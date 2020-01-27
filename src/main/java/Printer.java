1cc4c843a03725471ea451d4aa4524cec2fcd3df
Emma <emmagray183@gmail.com>
2020-01-27 18:37:28 +0000

    extension class and tests

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,29 @@
 public class Printer {
+
+    private int paper;
+    private int toner;
+
+    public Printer(int paper, int toner) {
+        this.paper = paper;
+        this.toner = toner;
+    }
+
+    public int getPaper() {
+        return paper;
+    }
+
+    public int getToner() {
+        return toner;
+    }
+
+    public String print(int pages, int copies){
+        int sheets = pages * copies;
+        if (sheets <= this.paper && sheets <= this.toner){
+            this.paper -= sheets;
+            this.toner -= sheets;
+            return "Printing complete";
+        } else {
+            return "There is not enough paper or toner to print this.";
+        }
+    }
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,2 +1,40 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PrinterTest {
+
+    private Printer printer;
+
+    @Before
+    public void before(){
+        printer = new Printer(500, 500);
+    }
+
+    @Test
+    public void hasPaper(){
+        assertEquals(500, printer.getPaper());
+    }
+
+    @Test
+    public void hasToner(){
+        assertEquals(500, printer.getToner());
+    }
+
+    @Test
+    public void paperDecreases(){
+        printer.print(5, 10);
+        assertEquals(450, printer.getPaper());
+    }
+
+    @Test
+    public void tonerDecreases(){
+        printer.print(5, 10);
+        assertEquals(450, printer.getToner());
+    }
+
+    @Test public void tooManyPages(){
+        assertEquals("There is not enough paper or toner to print this.", printer.print(100, 10));
+    }
 }
