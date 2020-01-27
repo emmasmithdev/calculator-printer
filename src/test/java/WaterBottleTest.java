b91ead9557a7d9d527c69f1ab28f84a9c529e6b5
Emma <emmagray183@gmail.com>
2020-01-27 18:15:53 +0000

    mvp classes and tests

--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,2 +1,38 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+
+    private WaterBottle bottle;
+
+    @Before
+    public void before() {
+        bottle = new WaterBottle();
+    }
+
+    @Test
+    public void hasVolume(){
+        assertEquals(100, bottle.getVolume());
+    }
+
+    @Test
+    public void volumeDecreases(){
+        bottle.drink();
+        assertEquals(90, bottle.getVolume());
+    }
+
+    @Test
+    public void emptyBottle(){
+        bottle.empty();
+        assertEquals(0, bottle.getVolume());
+    }
+
+    @Test
+    public void fillBottle(){
+        bottle.fill();
+        assertEquals(100, bottle.getVolume());
+    }
 }
+
